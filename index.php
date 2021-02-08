1e73e94daeacffcfd7bab18fffc833a550323014
bradleysigma <42644678+bradleysigma@users.noreply.github.com>
2021-02-08 12:12:51 +1000

    Link to GitHub

--- index.php
+++ index.php
@@ -173,7 +173,7 @@
 </article>
 <footer>
 <p><i>Twilight Imperium</i> &copy; Fantasy Flight Games. Not Affiliated.</p>
-<p>This website is managed through GitHub. Please submit any suggestions there.</p>
+<p>This website is managed through <a href="https://github.com/bradleysigma/tirules">GitHub</a>. Please submit any suggested changes there.</p>
 </footer>
 </body>
 </html>
--- suffix.php
+++ suffix.php
@@ -1,7 +1,7 @@
 <footer>
 <p><a href="/./" style="font-size:2em;">Home</a></p>
 <p><i>Twilight Imperium</i> &copy; Fantasy Flight Games. Not Affiliated.</p>
-<p>This website is managed through GitHub. Please submit any suggestions there.</p>
+<p>This website is managed through <a href="https://github.com/bradleysigma/tirules">GitHub</a>. Please submit any suggested changes there.</p>
 </footer>
 </body>
 </html>
