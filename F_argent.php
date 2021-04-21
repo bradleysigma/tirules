1ff16d2f752535d700cdbfd5ef046f3d0bcb1c5e
bradleysigma <42644678+bradleysigma@users.noreply.github.com>
2021-04-21 22:37:52 +1000

    Update F_argent.php

--- F_argent.php
+++ F_argent.php
@@ -13,7 +13,7 @@
     <li>The Argent player may choose a damaged ship. This will have no effect.</li>
     <li>The chosen ship becomes damaged, it does not use its <sc>Sustain Damage</sc> ability. As such, effects that trigger when a ship unses its <sc>Sustains Damage</sc> ability, such as the <i>Direct Hit</i> or <i>Reflective Shielding</i> action cards, cannot be played.</li>
     <li>If the Argent player plays the <i>Waylay</i> action card, hits in excess of their opponent&rsquo;s fighters will first cause chosen ships to become damaged, then those same hits must still be be assigned.</li>
-    <ol><li>For example, say the opponent has theee fighters and three dreadnoughts, undamaged. The Argent player plays <a>Waylay</a>, and rolls five hits. This allows them to cause two of the dreadnoughts to become damaged. The opponenet then has to assign all five hits. The final dreadnought may use its <sc>Sustain Damage</sc> ability to cancel one of those hits.</li></ol>
+    <ol><li>For example, say the opponent has three fighters and three dreadnoughts, undamaged. The Argent player plays <a>Waylay</a>, and rolls five hits. This allows them to cause two of the dreadnoughts to become damaged. The opponent then has to assign all five hits. The final dreadnought may use its <sc>Sustain Damage</sc> ability to cancel one of those hits.</li></ol>
     </ol>
 
 <h1>Strike Wing Ambuscade <sub>(Promissory Note)</sub></h1>
