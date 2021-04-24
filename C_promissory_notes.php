9e8ac42095f106348db2b4aa4c326310608854e3
bradleysigma <42644678+bradleysigma@users.noreply.github.com>
2021-04-24 14:21:20 +1000

    Update C_promissory_notes.php

--- C_promissory_notes.php
+++ C_promissory_notes.php
@@ -15,8 +15,8 @@
 <h1>Ceasefire</h1>
     <ol class="note">
     <li>The active player may still use the <sc>Space Cannon</sc> ability of any units during the <b>Space Cannon Offense</b> step of the tactical action.</li>
-    <li>If the active player already has units with capacity in the active system, they may transport them into the space area of that system. Those units may then be committed to invade a planet during this tactical action.</li>
-    <li>If a player has moved units into the active system before their <i>Ceasefire</i> is played, those units will remain in the active system.</li>
+    <li>If a player has units in the active system from before the tactical action in which their <i>Ceasefire</i> is played, those units will remain in the active system.</li>
+    <li>If the active player already has units with capacity in the active system, they may transport ground forces on planets in that system into the space area of that system. Those units may then be committed to invade a planet during this tactical action.</li>
     <li>If a system containing only a player&rsquo;s ground forces or structures is activated, that player may still play the active player&rsquo;s <i>Ceasefire</i>.</li>
     <li><i>Ceasefire</i> does not prevent a player from producing or placing units in the active system.</li>
     </ol>
