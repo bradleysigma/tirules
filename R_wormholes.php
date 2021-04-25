94ea82079742b16fd7f79a702dce0ccec1a475ce
bradleysigma <42644678+bradleysigma@users.noreply.github.com>
2021-04-25 18:03:10 +1000

    Update R_wormholes.php

--- R_wormholes.php
+++ R_wormholes.php
@@ -5,13 +5,13 @@
 <style> .lrr > li:before, .lrr > h2 > li:before {content: '101.' counter(item, decimal);} </style>
 <p>Some systems contain wormholes. Systems that contain identical wormholes are adjacent.</p>
     <ol class="lrr">
-	<li>There are two basic types of wormholes: alpha and beta.</li>
+    <li>There are two basic types of wormholes: alpha and beta.</li>
     <li>If a player has a PDS unit upgrade technology, they can use the <sc>Space Cannon</sc> abilities of their PDS units through wormholes.</li>
     <li>Players can be neighbors and perform transactions through wormholes.</li>
     <li>There are two advanced types of wormhole: delta and gamma. These wormholes follow all other wormhole rules.</li>
     <ol>
-	    <li>The delta wormholes are present on the Creuss Gate system tile and the Ghosts of Creuss home system tile.</li>
-    	<li>The gamma wormholes are present on the wormhole nexus and can be discovered during exploration.</li>
+        <li>The delta wormholes are present on the Creuss Gate system tile and the Ghosts of Creuss home system tile.</li>
+        <li>The gamma wormholes are present on the wormhole nexus and can be discovered during exploration.</li>
     </ol>
     </ol>
 
@@ -19,6 +19,7 @@
     <ol class="note">
     <li>A system with a wormhole is not an anomaly.</li>
     <li>If a system contains multiple wormholes of the same type, those are treated as separate wormholes.</li>
+    <li>A system cannot be adjacent to itself, even if it has multiple wormholes in it.</li>
     <li>For more information on how the Creuss interact with wormholes, see <a href="/F_creuss">their faction notes page</a>.</li>
     </ol>
 
