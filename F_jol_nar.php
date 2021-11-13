7312ef17ce7da1329c11518d07cecf6d9f693d3b
bradleysigma <42644678+bradleysigma@users.noreply.github.com>
2021-11-13 20:17:02 +1000

    Update F_jol_nar.php

--- F_jol_nar.php
+++ F_jol_nar.php
@@ -26,6 +26,12 @@
         <li>If the active player received the <i>Research Agreement</i> on a previous turn, they may play it and immediately perform a transaction with the Jol&ndash;Nar player to regain it. They may then play it when the Jol&ndash;Nar player researches their next technology.</li>
     </ol>
     <li>Any deal between the Jol&ndash;Nar player and another player regarding when <i>Research Agreement</i> will be played, if made before <i>Research Agreement</i> is exchanged, is non&ndash;binding.</li>
+    <li>If a faction has a faction&ndash;specific unit upgrade as one of their faction technologies, they cannot gain tht unit upgrade from <i>Research Agreement</i> when the Jol&ndash;Nar player researches the generic version of that unit upgrade.</li>
+    <li><i>Research Agreement</i> may be triggered when the Jol&ndash;Nar player reseaches a technology outside of the <i>Technology</i> strategy card. For example, it may be triggered by the <i>Divert Funding</i> action card, the <i>Focused Research</i> action card, the <i>Reveal Prototype</i> action card, the <i>Technology Rider<i> action card, the <i>Ixthian Artifact</i> agenda, the <i>Wormhole Research</i> agenda, or the <i>Enigmatic Device</i> exploration card.</li>
+    <ol>
+        <li><i>Research Agreement</i> cannot be triggered by a game effect that instructs the Jol&ndash;Nar player to directly gain a technology, without researching. For example, it cannot be triggered by the <i>Fire of the Gashlai<i> Muaat faction promissory note, the <i>Plagiarize</i> action card, the <i>Research Grant Reallocation</i> agenda, or the <i>Maw of Worlds</i> relic.</li>
+        <li><i>Research Agreement</i> cannot be triggered when the Jol&ndash;Nar player resolves <i>Genetic Memory</i>.</li>
+	</ol>
     </ol>
 
 <h1>E&ndash;Res Siphons <sub>(Technology)</sub></h1>
@@ -55,7 +61,7 @@
     <li>The Ta Zern may be used to reroll dice for an Infantry II resurrection ability.</li>
     </ol>
 
-<h1>Rin, The Master&rsquo;s Legacy <sub>(Hero)</sub></h1>
+<h1>Rin, The Master&rsquo;s Legacy &mdash; Genetic Memory<sub>(Hero)</sub></h1>
     <ol class="note">
     <li>The Jol&ndash;Nar player does not have to meet the prerequisites of the technologies they acquire.</li>
     <li>All replacements happen simultaneously. If this ability returns a specific technology, it cannot then be regained via this ability.</li>
