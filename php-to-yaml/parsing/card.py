9814cece659106f5a7267f468a200f5a99ef3daa
everlastingcrown <>
2025-04-17 21:03:01 +1000

    chore: add basic parsing of child rules

--- php-to-yaml/parsing/card.py
+++ php-to-yaml/parsing/card.py
@@ -1,16 +1,19 @@
-from typing import List
+from typing import List, Union
 from bs4 import BeautifulSoup
 from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class Rule:
     rule: str
-    children_rules: List['Rule']
+
+@dataclass(frozen=True)
+class Rules:
+    rules: List[Union[Rule, 'Rules']]
 
 @dataclass(frozen=True)
 class Card:
     name: str
-    rules: List[Rule]
+    rules: Rules
 
 @dataclass(frozen=True)
 class Cards:
@@ -20,13 +23,19 @@ def parse_card_page(soup: BeautifulSoup) -> List[Card]:
     cards = []
     for card in soup.find("article").find_all("h1"):
         name = card.text
-
-        # TODO: Need to handle lists within lists
-        rules = []
-        for rule in card.find_next_sibling("ol").find_all("li"):
-            rules.append(Rule(rule.text, []))
+        list_parent = card.find_next_sibling("ol")
+        rules = [] if not list_parent else [parse_rule(list_parent)]
         
-        cards.append(Card(name, rules))
+        cards.append(Card(name, Rules(rules)))
     
     return Cards(cards)
-        
+
+def parse_rule(rule: BeautifulSoup) -> Union[Rule, Rules]:      
+    if rule.name == "li":
+        return Rule(rule.text)
+    elif rule.name == "ol":
+        child_rules = rule.find_all(recursive=False)
+        rules = [parse_rule(child_rule) for child_rule in (child_rules if child_rules else [])]
+        return Rules(rules)
+    else:
+        raise Exception(f"Unsupported tag {rule.name} in rules")
