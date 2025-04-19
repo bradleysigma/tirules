b49b12ad9f8ab822ffbc867746a520258202f144
everlastingcrown <>
2025-04-19 15:02:28 +1000

    refactor: standardise generation of notes

--- php-to-yaml/convert.py
+++ php-to-yaml/convert.py
@@ -1,3 +1,4 @@
+from typing import Dict
 from bs4 import BeautifulSoup
 from parsing.index import parse_index_page
 from parsing.cards import parse_card_page
@@ -8,6 +9,18 @@ from html import unescape
 
 def format_name(name: str) -> str:
     return name.lower().replace(" ", "_")
+
+def generate_notes(notes: Dict[str, str], output_folder: str):
+    for name, path in notes.items():
+        # Open a component page and parse it
+        with open(path) as f:
+            # Unescape escape characters before parsing so that they appear
+            # in their correct form in YAML
+            notes_page = BeautifulSoup(unescape(f.read()), features="html.parser")
+
+            # For debugging purposes, output the pages
+            with open(f"output/{output_folder}/{format_name(name)}.yaml", "w", encoding="utf-8") as c:
+                c.write(to_yaml_str(parse_card_page(notes_page)))
 
 if __name__ == '__main__':
     # Create the "output" directory (and other directories) if they don't exist
@@ -31,25 +44,7 @@ if __name__ == '__main__':
             yaml.dump(root, f)
     
     # Generate the YAML files for the component notes
-    for name, path in root["Component Notes"].items():
-        # Open a component page and parse it
-        with open(path) as f:
-            # Unescape escape characters before parsing so that they appear
-            # in their correct form in YAML
-            component_page = BeautifulSoup(unescape(f.read()), features="html.parser")
-
-            # For debugging purposes, output the pages
-            with open(f"output/components/{format_name(name)}.yaml", "w", encoding="utf-8") as c:
-                c.write(to_yaml_str(parse_card_page(component_page)))
+    generate_notes(root["Component Notes"], "components")
     
     # Generate the YAML files for the faction notes
-    for name, path in root["Faction Notes"].items():
-        # Open a component page and parse it
-        with open(path) as f:
-            # Unescape escape characters before parsing so that they appear
-            # in their correct form in YAML
-            faction_page = BeautifulSoup(unescape(f.read()), features="html.parser")
-
-            # For debugging purposes, output the pages
-            with open(f"output/factions/{format_name(name)}.yaml", "w", encoding="utf-8") as c:
-                c.write(to_yaml_str(parse_card_page(faction_page)))
+    generate_notes(root["Faction Notes"], "factions")
