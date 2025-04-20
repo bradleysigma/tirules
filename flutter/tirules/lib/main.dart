e595a28ac14e4b48fa8daa106c4a43a2e97b4ec8
everlastingcrown <>
2025-04-20 12:55:40 +1000

    feat: add basic page generator for some rules

--- flutter/tirules/lib/main.dart
+++ flutter/tirules/lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:yaml/yaml.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
 class RulesWidget extends StatefulWidget {
-  const RulesWidget({super.key});
+  final String configPath;
+
+  const RulesWidget({super.key, required this.configPath});
 
   @override
   State<RulesWidget> createState() => _RulesWidgetState();
 }
 
 class _RulesWidgetState extends State<RulesWidget> {
-  String yamlContent = "Loading...";
+  Map rules = {};
   
   @override
   void initState() {
@@ -22,19 +25,43 @@ class _RulesWidgetState extends State<RulesWidget> {
   }
 
   Future<void> loadContent() async {
-    final content = await rootBundle.loadString("assets/rules/root.yaml");
+    final yamlString = await rootBundle.loadString(widget.configPath);
+    final yamlMap = loadYaml(yamlString) as Map;
 
     setState(() {
-      yamlContent = content;
+      rules = yamlMap;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [Text(yamlContent)]
+    List<Widget> cards = rules.containsKey("cards") ? [for (var card in rules["cards"]) generateCard(card)] : [];
+
+    return Column(
+      children: cards,
     );
   }
+
+  Widget generateCard(Map card) {
+    return Column(children: [
+      Text(
+        card["name"] ?? "???", 
+        textScaler: TextScaler.linear(2)
+      ),
+      if (card.containsKey("rules")) generateRules(card["rules"])
+    ]);
+  }
+
+  Widget generateRules(Map rules) {
+    var rulesWidgets = [for (var order in rules.keys) generateRule(order, rules[order])];
+
+    return Column(children: rulesWidgets);
+  }
+
+  Widget generateRule(String order, Map rule) {
+    return Text("$order: ${rule['rule']}");
+  }
+
 }
 
 class MainApp extends StatelessWidget {
@@ -44,16 +71,8 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: RulesWidget(),
-        ),
+        body: RulesWidget(configPath: "assets/rules/components/agendas.yaml"),
       ),
     );
   }
-}
-
-Future<String> loadYamlFile() async {
-  final yamlString = await rootBundle.loadString('../../output/root.yaml');
-
-  return yamlString;
 }
